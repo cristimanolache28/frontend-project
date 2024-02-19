c0a29f8865df3f377399d5defc047225c6f192f8
Cristian Manolache <cristianmanolache0@gmail.com>
2024-02-20 00:37:28 +0200

    create component through function and class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       My Todo Application
+      <FirstComponenet></FirstComponenet>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
+      <ForthComponent></ForthComponent>
     </div>
   );
+}
+
+function FirstComponenet() {
+  return (
+    <div className="First Compoent">First Component</div>
+  )
+}
+
+function SecondComponent() {
+  return (
+    <div className='Second Component'>Second Compoent</div>
+  )
+}
+
+class ThirdComponent extends Component {
+  render() {
+    return (
+      <div className='Third Component'>Third Component</div>
+    )
+  }
+}
+
+class ForthComponent extends Component {
+  render() {
+    return (
+      <div className='Forth Component'>Forth Component</div>
+    )
+  }
 }
 
 export default App;
